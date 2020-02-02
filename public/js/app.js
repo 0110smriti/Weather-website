fcb1b8c9c6f3ab79516a726d60b53e2cac954f86
Smriti <chaturvedi.smriti1@gmail.com>
2020-02-02 16:29:59 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const p1 = document.querySelector('#p1')
+const p2 = document.querySelector('#p2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    p1.textContent = 'Loading...'
+    p2.textContent = ''
+
+    const location = search.value
+    fetch('http://localhost:3000/weather?address='+ location).then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            p1.textContent = data.error
+        }else{
+            p1.textContent = data.forecast
+            p2.textContent = data.location
+        }
+    })
+})
+})
---
+++ utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/e9f1f0fd2883f2b8297347ccc3301773/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '?units=si&lang=en'
+
+    request({url, json: true}, (error, {body}) => {
+        if(error){
+            callback('Please Check your network connection', undefined)
+        }
+        else if(body.error){
+            callback('Unable to connect to server.', undefined)
+        }
+        else{
+            const res = body
+            const data = res.daily.data[0].summary + 'It is currenlty '+ res.currently.temperature + ' degrees out. There is a '+ res.currently.precipProbability +'% chance of rain.'
+            callback(undefined, data)
+        }
+    })
+}
+
+module.exports =  forecast
